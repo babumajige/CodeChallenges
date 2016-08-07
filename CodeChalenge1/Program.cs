746eea09b1ca2ad239bcc02740c220683f5056c2
babumajige <babumajige82@gmail.com>
2016-08-08 01:26:25 +0530

    CodeChallenge1

---
+++ CodeChalenge1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge1
+{
+    public class CodeChallengeOne
+    {
+        public static void Main(string[] args)
+        {
+            var inputValues = Console.ReadLine();
+            if (inputValues != null)
+            {
+                String[] elements = inputValues.Split(',');                 
+                int[] numbers = Array.ConvertAll(elements, Int32.Parse);  
+                bool result = CheckSum(numbers);                             //check sum of left and right numbers of index
+                Console.WriteLine(result ? "TRUE" : "FALSE");               
+            }
+        }
+
+        public static bool CheckSum(int[] nums)
+        {
+            int leftIndex = 0;
+            int rightIndex = nums.Length - 1;
+
+            long leftSum = nums[leftIndex];
+            long rightSum = nums[rightIndex];
+
+            while (leftIndex != rightIndex)
+            {
+                if (leftSum < rightSum)
+                {
+                    leftIndex++;
+                    leftSum += nums[leftIndex];
+                }
+                else 
+                {
+                    rightIndex--;
+                    rightSum += nums[rightIndex];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ CodeChallenge1Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeChallenge1;
+
+namespace CodeChallenge1Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var enteredNumbrs = new int[4] { 1, 2, 3,4 };
+            var result = CodeChallengeOne.CheckSum(enteredNumbrs);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var enteredNumbrs = new int[4] { 11, 20, 8, 31 };
+            var result = CodeChallengeOne.CheckSum(enteredNumbrs);
+            Assert.IsTrue(result);
+        }
+    }
+}
